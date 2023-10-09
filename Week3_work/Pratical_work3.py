172cff4e526891946393380848ed50760f46d95c
c3666218 <J.Tew@student.leedsbeckett.ac.uk>
2023-10-09 14:26:52 +0100

    Update README.md

---
+++ Week3_work/Pratical_work3.py
@@ -0,0 +1,70 @@
+#Task 1
+#age = 18
+
+#if age >=18 or age <=21 or age <=31 :
+    #print ("True")
+
+#else:
+    #print ("False")
+
+
+#Task 2
+
+#age = 30
+
+#age >=18 and age <= 65
+#age < 18 or age > 65
+#not age > 18
+
+#age = int(input("Enter You Age:"))
+
+#if age > 100:
+	#print("you are very old,")
+	#print("well done!")
+
+#elif age > 80:
+	#print("you are fairly old")
+	#print("pretty good!")
+
+#elif age > 40:
+	#print("you are middle aged")
+	#print("never mind")
+
+#elif age >=30 or age <=40:
+	#print("You are not that old")
+#else:
+	#print("you are not very old - yet")
+
+
+#name = input("Enter your name:")
+
+#if name:
+	#print ("True")
+
+#else:
+	#print ("False")
+
+#age = 19
+
+#if age >= 18:
+#	print ( "you are an adult" if age > 18 else "you are not an adult yet")
+
+#names = [ "james", "lucy", "george", "hugo"]
+
+#for n in names:
+#	print (names)
+
+#num = 2
+#power = 2
+
+#for n in range (8):
+
+#	print (num, "to the power of",power, ("="), num ** power)
+#	num= n + 2
+#	power = power +2
+
+numbers = [10,20,30,90,200,30,22,11]
+
+for n in numbers 
+
+
