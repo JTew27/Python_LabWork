bcf0856ce85681d7da18cba39d791c1f186eac97
JTew27 <jamestew2707@gmail.com>
2023-11-19 16:43:51 +0000

    Hwk

--- Week5-Scripts and Modules/Higher or Lower.py
+++ Week5-Scripts and Modules/Higher or Lower.py
@@ -1,10 +1,10 @@
 import random
 
-number = random.randint(1,100)
+number = random.randint(1,20)
 count = 10
 
 while count != 0:
-    guess = int(input("Guess a number between 1 and 100:"))
+    guess = int(input("Guess a number between 1 and 20:"))
     if guess == number:
         print("you guessed correctly")
         break
@@ -14,3 +14,4 @@ while count != 0:
 
 if count == 0:
     print ("You Failed to Guess!")
+    print ("The number was", number)
