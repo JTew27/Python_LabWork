5391544c4ab492553de2c1dc33dee8382a64849c
JTew27 <jamestew2707@gmail.com>
2023-11-26 17:21:07 +0000

    Hwk

---
+++ Assignment Portfolio/Task1 - PizzaPrices.py
@@ -0,0 +1,30 @@
+
+def discount():
+
+
+
+def order():
+
+
+    total = 0
+    pizza = 12
+    DeliveryCost = 2.50
+    app = False
+
+    order = int(input("How many pizzas have been ordered?  "))
+    day = input("What day is it?  ")
+    delivery = input("Is delivery required?  ")
+    AppUse = input("Has the app been used to place the order")
+
+ if order > 5:
+     discount()
+
+
+
+
+
+
+
+# Main Program
+print ("BPP Pizza Price Calculator")
+print ("__________________________")
